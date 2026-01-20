b0cf642b03878b5f5c23677f1dbb9b7c63cafe98
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-20 18:32:56 +0300

    add some features in userProfile page

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-
+import { UserContext } from "./UserContext";
+import { useContext } from "react";
 function UserProfile() {
+  const userData = useContext(UserContext);
   return <div>UserProfile</div>;
 }
 
