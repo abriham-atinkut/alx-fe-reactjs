3bea9c203799191b137dda81de546a90d5b57e81
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-20 19:09:50 +0300

    implement routes in my-company

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
