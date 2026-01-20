c81c3ef3b1b8300c6a53ef11a3136234dd1ea8f3
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-20 19:18:33 +0300

    style nav

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,31 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="nav">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/services">Services</Link>
-      <Link to="/contact">Contact</Link>
+    <div className="nav" style={{ backgroundColor: "black", padding: "5vh" , justifyContent: "center"}}>
+      <Link
+        to="/"
+        style={{ color: "blue", padding: "10px", textDecoration: "none" }}
+      >
+        Home
+      </Link>
+      <Link
+        to="/about"
+        style={{ color: "blue", padding: "10px", textDecoration: "none" }}
+      >
+        About
+      </Link>
+      <Link
+        to="/services"
+        style={{ color: "blue", padding: "10px", textDecoration: "none" }}
+      >
+        Services
+      </Link>
+      <Link
+        to="/contact"
+        style={{ color: "blue", padding: "10px", textDecoration: "none" }}
+      >
+        Contact
+      </Link>
     </div>
   );
 }
