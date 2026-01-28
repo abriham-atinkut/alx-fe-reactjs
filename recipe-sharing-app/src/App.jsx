550fcd52ba7bd79a13ff7533a8ab810c65e48e51
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-28 20:20:11 +0300

    fix: two line of code #1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import AddRecipeForm from "./components/AddRecipeForm ";
-import RecipeList from "./components/RecipeList ";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
 
 function App() {
   return (
