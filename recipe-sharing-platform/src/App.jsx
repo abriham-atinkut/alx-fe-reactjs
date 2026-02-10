1ae71ec25ed55aa45a67c0c89725394171af0d11
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-10 21:32:34 +0300

    create recipe form component with tailwind stryling

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
+import AddRecipeForm from "./components/AddRecipeForm";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/:id" element={<RecipeDetail />} />
+          <Route path="/form" element={<AddRecipeForm />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const AddRecipeForm = () => {
+  const [title, setTitle] = useState("");
+  const [ingredient, setIngredient] = useState("");
+  const [prepatation, setPreparation] = useState("");
+
+  const submitChecker = (event) => {
+    event.preventDefault();
+    
+    setTitle("");
+    setIngredient("");
+    setPreparation("");
+  };
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <form onSubmit={submitChecker} className="p-6 rounded shadow-lg">
+          <h2 className="text-2xl mb-4 text-center font-bold">Add Recipe</h2>
+          <div className="mb-4">
+            <label className="block">Title:</label>
+            <input
+              type="text"
+              className="border-2 w-full focus:border-green-400 focus:outline-none rounded-md"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block">Ingredients:</label>
+            <textarea
+              name="ingredients"
+              className="border-2  w-full focus:border-green-400 focus:outline-none rounded-md"
+              value={ingredient}
+              onChange={(e) => setIngredient(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <label className="block">Preparation:</label>
+            <textarea
+              typeof="text"
+              name="preparation"
+              className="border-2  w-full focus:border-green-400 focus:outline-none rounded-md"
+              value={prepatation}
+              onChange={(e) => setPreparation(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className=" bg-blue-600 hover:bg-blue-500 font-semibold rounded-md  py-2 px-4 text-white"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AddRecipeForm;
