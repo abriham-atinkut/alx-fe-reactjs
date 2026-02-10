143b379f2d0576427527b9feb44e2e7349106d87
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-10 15:12:29 +0300

    building home page card and style using tailwind

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
-      <h1></h1>
-      <h1 className="text-blue-700">Hello</h1>
+      <HomePage />
     </>
   );
 }
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const HomePage = () => {
+  const [recipes, setRecipe] = useState([]);
+  useEffect(() => {
+    fetch("../../src/data.json")
+      .then((res) => res.json())
+      .then((data) => setRecipe(data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(recipes);
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-4 sm:gap-x-4 m-2">
+        {recipes.map((recipe) => (
+          <div
+            key={recipe.id}
+            className="border-2 bg-slate-200 py-4 hover:shadow-lg  hover:scale-105 hover:transition-transform hover:duration-500 hover:ease-in-out "
+          >
+            <img
+              src={recipe.image}
+              alt=""
+              className="mx-auto  text-center rounded-lg h-18 w-18 md:w-32 md:h-32"
+            />
+            <div className="mx-6 md:my-auto">
+              <h1 className="font-bold"> {recipe.title} </h1>
+              <p> {recipe.summary} </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
