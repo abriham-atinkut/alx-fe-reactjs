534d3b145f8c780c02f55af8d901cc30dfd1252e
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-10 17:42:04 +0300

    change where BrowserRouter is implimented

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:id" element={<RecipeDetail />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:id" element={<RecipeDetail />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
