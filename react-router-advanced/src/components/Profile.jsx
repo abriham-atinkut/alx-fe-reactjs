9b91a09798ee2479d23549579c8299af8bf007f1
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-24 16:53:41 +0300

    change file name from react-router-dom to react-router-advanced

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
+const Profile = () => {
+  return (
+    <>
+      <div>
+        <h2>Profile Page</h2>
+        <nav>
+          <Link to="details">Detail</Link>
+          <Link to="settings">Setting</Link>
+        </nav>
+
+        <Routes>
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="setting" element={<ProfileSettings />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
